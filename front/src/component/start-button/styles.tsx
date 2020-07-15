f802dc58f11317aaf72321aa4c0f4e8533373f0f
박진용 <jinyoung1997@gmail.com>
2020-07-15 23:34:32 +0900

    main design 0.1

--- front/src/component/start-button/styles.tsx
+++ front/src/component/start-button/styles.tsx
@@ -26,6 +26,10 @@ export const StartButtonContainer = styled.div`
     background-color: ${THEME_COLOR.PRIMARY};
     transform: scale(1.2) perspective(0.7rem);
   }
+  &:active {
+    transform: translateY(-1px);
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+  }
 `;
 
 export const ButtonText = styled.div`
--- front/src/container/quiz-category-list/styles.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components/macro";
-
-import { THEME_COLOR } from "../../constant";
-
-export const QuizCategoryList = styled.ul`
-  margin: 0;
-  padding: 0;
-  border-bottom: 1px solid ${THEME_COLOR.PRIMARY};
-`;
